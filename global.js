270387c5353132aaee761db1ae424313228e3990
Giorgi <mebonia751@gmail.com>
2024-12-04 02:22:32 +0400

    blog-app

---
+++ global.js
@@ -0,0 +1,12 @@
+
+// setTimeout(() => {
+//     console.log('in the timeout')
+//     clearInterval(int)
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log('interval done');
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
---
+++ people.js
@@ -0,0 +1,19 @@
+// const people = [
+//     'giorgi',
+//     'luka',
+//     'ilia',
+//     'shorena',
+// ];
+
+// const ages = [
+//     20,
+//     25,
+//     30,
+//     35,
+// ];
+
+// console.log(people);
+
+// module.exports = {
+//     people, ages
+// }
